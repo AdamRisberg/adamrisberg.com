8d6d27e3c9f80be1b9d74a94c618bb29a273db9e
Adam Risberg <myniztan@hotmail.com>
2019-04-02 01:36:06 -0500

    Refactor contact form to use hooks

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
 import { css } from "emotion";
 
@@ -41,138 +41,136 @@ const buttonStyle = css`
   }
 `;
 
-class ContactForm extends Component {
-  state = {
-    name: "",
-    email: "",
-    message: "",
-    honeypot: false,
-    buttonEnabled: true,
-    error: false,
-    successErrorMessage: ""
-  };
+function axiosPostReducer(state, action) {
+  switch (action.type) {
+    case "sending":
+      return {
+        sending: true,
+        response: "",
+        error: false
+      };
+    case "success":
+      return {
+        sending: false,
+        response: "Message successfully sent!",
+        error: false
+      };
+    case "failed":
+      return {
+        sending: false,
+        response: "Message could not be sent.",
+        error: true
+      };
+    default:
+      throw new Error(`Unknown type: ${action.type}`);
+  }
+}
 
-  onTextChange = key => e => {
-    const value = e.target.value;
-    this.setState({ [key]: value });
-  };
+function useAxiosPost(url, body) {
+  const [state, dispatch] = React.useReducer(axiosPostReducer, {
+    sending: false,
+    response: "",
+    error: false
+  });
 
-  onCheckboxChange = () => {
-    this.setState(state => ({ honeypot: !state.honeypot }));
-  };
+  React.useEffect(() => {
+    if (body) {
+      dispatch({ type: "sending" });
 
-  handleSubmit = e => {
+      axios
+        .post(url, body)
+        .then(_ => dispatch({ type: "success" }))
+        .catch(_ => dispatch({ type: "failed" }));
+    }
+  }, [body]);
+
+  return state;
+}
+
+function ContactForm() {
+  const [body, setBody] = React.useState("");
+  const { sending, response, error } = useAxiosPost(
+    "https://usebasi.com/f/ddbe35ae8070.json",
+    body
+  );
+
+  function handleSubmit(e) {
     e.preventDefault();
-    if (this.state.honeypot) return;
-    this.setState({ buttonEnabled: false });
+    const data = new FormData(e.target);
+    if (data.get("agree-to-terms")) return;
 
-    const data = {
-      name: this.state.name,
-      email: this.state.email,
-      message: this.state.message
-    };
-
-    axios
-      .post("https://usebasi.com/f/ddbe35ae8070.json", data)
-      .then(_ => {
-        this.setState({
-          error: false,
-          successErrorMessage: "Message successfully sent!",
-          buttonEnabled: true
-        });
+    setBody(
+      JSON.stringify({
+        name: data.get("name"),
+        email: data.get("email"),
+        message: data.get("message")
       })
-      .catch(_ => {
-        this.setState({
-          error: true,
-          successErrorMessage: "Error: Message could not be sent.",
-          buttonEnabled: true
-        });
-      });
-  };
-
-  render() {
-    const {
-      name,
-      email,
-      message,
-      honeypot,
-      buttonEnabled,
-      successErrorMessage,
-      error
-    } = this.state;
-
-    return (
-      <form
-        className={css`
-          text-align: right;
-        `}
-        onSubmit={this.handleSubmit}
-      >
-        <label className={labelStyle}>
-          Name
-          <input
-            className={inputStyle}
-            id="name"
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.onTextChange("name")}
-            required
-          />
-        </label>
-        <label className={labelStyle}>
-          Email Address
-          <input
-            className={inputStyle}
-            id="email"
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.onTextChange("email")}
-            required
-          />
-        </label>
-        <label className={labelStyle}>
-          Message
-          <textarea
-            className={inputStyle}
-            id="message"
-            name="message"
-            rows="5"
-            value={message}
-            onChange={this.onTextChange("message")}
-            required
-          />
-        </label>
-        <input
-          style={{ display: "none" }}
-          id="agree-to-terms"
-          tabIndex="-1"
-          autoComplete="off"
-          type="checkbox"
-          name="agree-to-terms"
-          checked={honeypot}
-          onChange={this.onCheckboxChange}
-        />
-        {successErrorMessage && (
-          <div
-            className={css`
-              text-align: left;
-              margin-bottom: 10px;
-              padding: 5px 15px;
-              border: 2px solid ${error ? colors.errorLight : colors.successLight};
-              background-color: ${error ? colors.error : colors.success};
-            `}
-          >
-            {successErrorMessage}
-          </div>
-        )}
-        <button type="submit" className={buttonStyle} disabled={!buttonEnabled}>
-          {buttonEnabled ? "Send Message" : "Sending..."}
-        </button>
-      </form>
     );
   }
+
+  return (
+    <form
+      className={css`
+        text-align: right;
+      `}
+      onSubmit={handleSubmit}
+    >
+      <label className={labelStyle}>
+        Name
+        <input
+          className={inputStyle}
+          id="name"
+          type="text"
+          name="name"
+          required
+        />
+      </label>
+      <label className={labelStyle}>
+        Email Address
+        <input
+          className={inputStyle}
+          id="email"
+          type="email"
+          name="email"
+          required
+        />
+      </label>
+      <label className={labelStyle}>
+        Message
+        <textarea
+          className={inputStyle}
+          id="message"
+          name="message"
+          rows="5"
+          required
+        />
+      </label>
+      <input
+        style={{ display: "none" }}
+        id="agree-to-terms"
+        tabIndex="-1"
+        autoComplete="off"
+        type="checkbox"
+        name="agree-to-terms"
+      />
+      {response && (
+        <div
+          className={css`
+            text-align: left;
+            margin-bottom: 10px;
+            padding: 5px 15px;
+            border: 2px solid ${error ? colors.errorLight : colors.successLight};
+            background-color: ${error ? colors.error : colors.success};
+          `}
+        >
+          {response}
+        </div>
+      )}
+      <button type="submit" className={buttonStyle} disabled={sending}>
+        {sending ? "Sending..." : "Send Message"}
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
