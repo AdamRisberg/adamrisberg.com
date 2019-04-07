a12d9f36b99408e34e28e23916189173bf4a8d21
Adam Risberg <myniztan@hotmail.com>
2019-04-07 16:15:35 -0500

    Improve tab navigation in Portfolio section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -41,29 +41,32 @@ const cardStyle = css`
   max-width: 420px;
   margin: 0 auto;
   &:hover [data-project-content],
-  &:focus [data-project-content] {
+  &.focus [data-project-content] {
     opacity: 1;
     pointer-events: auto;
   }
-  &:hover [data-project-content] div:nth-of-type(1),
-  &:focus [data-project-content] div:nth-of-type(1) {
+  &.focus [data-project-content] {
+    transition: none;
+  }
+  &:hover [data-project-content] div:nth-of-type(1) {
     transform: translateY(0);
     transition: transform 0.3s 0.5s;
   }
-  &:hover [data-project-content] div:nth-of-type(2),
-  &:focus [data-project-content] div:nth-of-type(2) {
+  &:hover [data-project-content] div:nth-of-type(2) {
     transform: translateY(0);
     transition: transform 0.3s 0.4s;
   }
-  &:hover [data-project-content] div:nth-of-type(3),
-  &:focus [data-project-content] div:nth-of-type(3) {
+  &:hover [data-project-content] div:nth-of-type(3) {
     transform: translateY(0);
     transition: transform 0.3s 0.3s;
   }
-  &:hover [data-project-content] div:nth-of-type(4),
-  &:focus [data-project-content] div:nth-of-type(4) {
+  &:hover [data-project-content] div:nth-of-type(4) {
     transform: translateY(0);
     transition: transform 0.3s 0.5s;
+  }
+  &.focus [data-project-content] div {
+    transform: translateY(0);
+    transition: none;
   }
 `;
 
@@ -73,9 +76,20 @@ const preTransformStyle = css`
 `;
 
 function ProjectCard({ className = "", title, type, tech, image, links = [] }) {
+  const focusRef = React.useRef();
+
+  const onFocus = () => focusRef.current.classList.add("focus");
+  const onBlur = () => focusRef.current.classList.remove("focus");
+
   return (
     <div className={className}>
-      <div tabIndex="0" className={cardStyle}>
+      <div
+        ref={focusRef}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        tabIndex="0"
+        className={cardStyle}
+      >
         <img src={image} alt="React Cart" />
         <div className={contentStyle} data-project-content>
           <div
@@ -97,6 +111,8 @@ function ProjectCard({ className = "", title, type, tech, image, links = [] }) {
           >
             {links.map(link => (
               <a
+                onFocus={onFocus}
+                onBlur={onBlur}
                 aria-label={`${title} ${link.title}`}
                 key={`${title}-${link.title}`}
                 href={link.href}
